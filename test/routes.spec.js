60617f5bcfac914aa8a44d43f77d1325e0114987
Austin <joha0033@gmail.com>
2018-07-25 15:00:05 -0600

    updated tests

--- test/routes.spec.js
+++ test/routes.spec.js
@@ -4,13 +4,17 @@ process.env.NODE_ENV = 'test'
 const chai = require('chai')
 const should = chai.should()
 const chaiHttp = require('chai-http')
-const server = require('../app.js')
+const app = require('../app.js')
 
 chai.use(chaiHttp)
 
-describe('GET /api/v1/users/test', () => {
+describe('GET test', () => {
+	after(function (done) {
+        app.server.close();
+        done();
+    });
     it('return JSON, msg: test success', (done) => {
-    	chai.request(server)
+    	chai.request(app.server)
 	    .get('/api/v1/users/test')
 	    .end((err, res) => {
 			res.should.be.json
@@ -22,9 +26,13 @@ describe('GET /api/v1/users/test', () => {
 	})
 })
 
-describe('GET all users', () => {
-	it('returns an array of users', (done) => {
-		chai.request(server)
+describe('GET routes for users', () => {
+	after(function (done) {
+        app.server.close();
+        done();
+    });
+	it('/api/v1/users/all returns an array of users', (done) => {
+		chai.request(app.server)
 		.get('/api/v1/users/all')
 		.end((err, res) => {
 			res.body[0].should.have.property('id')
@@ -35,15 +43,13 @@ describe('GET all users', () => {
 			done()
 		})
 	})
-})
-
-describe('GET one user with projects', () => {
-	it('returns one user', (done) => {
-		chai.request(server)
+	it('/api/v1/users/Billy returns one user, Billy', (done) => {
+		chai.request(app.server)
 		.get('/api/v1/users/Billy')
 		.end((err, res) => {
 			res.body.should.have.property('id')
 			res.body.should.have.property('username')
+			res.body.username.should.equal('Billy')
 			res.body.should.have.property('password')
 			res.body.should.have.property('projects')
 			res.body.projects.should.be.a('array')
@@ -52,25 +58,21 @@ describe('GET one user with projects', () => {
 			done()
 		})
 	})
-})
-
-describe('GET one project with lists and tasks', () => {
-	it('returns one full project. List + Tasks', (done) => {
-		chai.request(server)
+	it('/api/v1/users/Billy/project/1 returns one full project with List + Tasks', (done) => {
+		chai.request(app.server)
 		.get('/api/v1/users/Billy/project/1')
 		.end((err, res) => {
-			console.log(res.body);
-			
 			res.body.should.have.property('id')
 			res.body.should.have.property('name')
 			res.body.should.have.property('description')
 			res.body.should.have.property('lists')
-			res.body.should.have.property('tasks')
+			res.body.lists[0].should.have.property('tasks')
 			res.body.lists.should.be.a('array')
-			res.body.tasks.should.be.a('array')
+			res.body.lists[0].tasks.should.be.a('array')
 			res.should.be.json
 			res.should.have.status(200);
 			done()
 		})
 	})
+
 })
